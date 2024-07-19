4e1d5c8a6f8ea597131526b91c02c0f84af5fa87
MatheusGCruz <matheuskaze@gmail.com>
2024-07-19 10:04:33 -0300

    First Send

---
+++ src/main/java/com/homeAssistantTelegram/HomeAssistantTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.homeAssistantTelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssistantTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAssistantTelegramApplication.class, args);
+	}
+
+}
